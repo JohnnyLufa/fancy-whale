48d23382101f61ad725bcb225c366d9fe6db4ed0
j.lin <j.lin@lufa.com>
2021-12-20 01:21:05 -0500

    Use local cache when pushing from local

--- push_bananas-local.sh
+++ push_bananas-local.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker buildx build --pull --no-cache --platform linux/amd64,linux/arm64 -t "$DOCKER_USER"/bananas:"$(date +"%Y%m%d")" -t "$DOCKER_USER"/bananas:latest --push -f ./bananas/Dockerfile.dev ./bananas/
+docker buildx build --pull --platform linux/amd64,linux/arm64 -t "$DOCKER_USER"/bananas:"$(date +"%Y%m%d")" -t "$DOCKER_USER"/bananas:latest --push -f ./bananas/Dockerfile.dev ./bananas/
