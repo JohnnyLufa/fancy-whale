654cb22519fddfb3c6f322381fe01d287dab285e
j.lin <j.lin@lufa.com>
2022-01-27 13:43:56 -0500

    Add python path to npm

--- build_python_node_builder-local.sh
+++ build_python_node_builder-local.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker buildx build -t johnnylinlufa/python_node_builder:latest --load -f ./rootabaga/Dockerfile.python_node_builder ./rootabaga/
+docker buildx build --platform=linux/arm64 -t johnnylinlufa/python_node_builder:latest --load -f ./rootabaga/Dockerfile.python_node_builder ./rootabaga/
